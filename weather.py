4ba1f33c6e8653b4e12896ac9ac3d7e8e7e3a31c
kajankowska <85677666+kajankowska@users.noreply.github.com>
2021-08-17 23:57:39 +0200

    update

--- weather.py
+++ weather.py
@@ -4,26 +4,13 @@ import datetime
 
 key = sys.argv[1]
 weatherdate = sys.argv[2]
-
-
-class Forecast:
-    def __init__(self, day, city):
-        self.day = datetime.datetime.strptime(sys.argv[2], "%Y-%m-%d").date()
-        self.city = city
-
-    def proper_day(self):
-        difference = self.day - datetime.datetime.today().date()
-        if difference.days > 16:
-            return False
-        if difference.days < 0:
-            return False
-        return True
+newdata = {}
 
 
 def api_data_downloading():
     url = "https://community-open-weather-map.p.rapidapi.com/forecast/daily"
 
-    querystring = {"q": "Warszawa", "lat": "Warszawa", "lon": "Warszawa", "cnt": "16", "units": "metric or imperial"}
+    querystring = {"q": "Warszawa", "lat": "35", "lon": "139", "cnt": "16", "units": "metric or imperial"}
 
     headers = {
         'x-rapidapi-key': key,
@@ -33,15 +20,32 @@ def api_data_downloading():
     response = requests.request("GET", url, headers=headers, params=querystring)
 
     rowdata = response.json()
-    newdata = {}
 
     for data in rowdata["list"]:
         dt = data["dt"]
-        print(dt)
         weather = data["weather"][0]["main"]
-        print(weather)
         newdata[dt] = weather
     return newdata
 
 
+def proper_day():
+    difference = datetime.datetime.strptime(sys.argv[2], "%Y-%m-%d").date() - datetime.datetime.today().date()
+    if difference.days > 16 or difference.days < 0:
+        print("Date outside the range.")
+        pass
+    else:
+        print("The weather forecast will be checked.")
+
+
+def weather_check():
+    if weatherdate in newdata == "Rain":
+        print("It will rain.")
+    if weatherdate in newdata == "Clouds":
+        print("I don't know (if it will rain).")
+    if weatherdate in newdata == "Clear":
+        print("It won't rain.")
+
+
+print(proper_day())
 print(api_data_downloading())
+print(weather_check())
